b16fee2026f398d788454e7044d33849c3b22fa8
irshad <irshad89434@gmail.com>
2025-01-13 11:20:04 +0530

    feat(prod): add rate limiter and morgan

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,26 @@ import { appRouter } from "./app/routes";
 import { errorHandler } from "./app/middlewares/error-handler";
 import { NotFoundError } from "./app/errors";
 import cookieParser from "cookie-parser"
+import morgan from "morgan"
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(json());
 app.use(cookieParser());
+app.use(morgan("combined"));
+
+// Configure rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100, 
+  standardHeaders: true, 
+  legacyHeaders: false, 
+  message: "Too many requests from this IP, please try again later.",
+});
+
+// Applying the rate limiter to all requests
+app.use(limiter);
 
 app.use("/api/v1", appRouter);
 
